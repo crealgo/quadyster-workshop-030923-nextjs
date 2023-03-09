f83fe38aa8b87981e0197ba709fe942e224a5249
Manas Chimpidi <manaschimpidi@gmail.com>
2023-03-09 13:32:36 -0600

    feat: add header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import {ThemeProvider, CssBaseline, createTheme} from '@mui/material'
+import {ThemeProvider, GlobalStyles, CssBaseline, createTheme, css} from '@mui/material'
 
 const defaultTheme = createTheme();
 
@@ -6,6 +6,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
+      <GlobalStyles styles={css`
+        body {
+          background-color: #CCCCCC
+        }
+      `} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,39 @@
 import Button from "@mui/material/Button"
 import { navigationItems, tableData, statistics, getProfile } from '../data'
+import styled from '@mui/material/styles/styled'
+import { IconButton, TextField } from "@mui/material"
+import { KeyboardArrowDown, NotificationsOutlined } from "@mui/icons-material"
+
+const HeaderBar = styled('header')`
+  display: flex;
+  background-color: white;
+  align-items: center;
+  padding-inline: 1rem; // 16px
+  padding-block: 0.5rem; // 8px
+
+  & .startContent {
+    flex-grow: 1;
+  }
+
+  & .endContent {}
+`
 
 export const Home = (props) => {
   console.log({props});
   return (
-    <div>
-     <Button>{'Hello'}</Button>
-    </div>
+    <>
+     <HeaderBar>
+      <div className='startContent'>
+        <TextField size='small' />
+      </div>
+      <div className="endContent">
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <Button endIcon={<KeyboardArrowDown />}>{props.userProfile.name}</Button>
+      </div>
+     </HeaderBar>
+    </>
   )
 }
 
